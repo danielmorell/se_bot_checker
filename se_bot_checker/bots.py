03d1c06f5421124e913f6cd568cf5a4621e5460b
Daniel Morell <daniel@cairntek.com>
2020-04-07 20:56:55 -0500

    Added lots of class and method documentation

--- se_bot_checker/bots.py
+++ se_bot_checker/bots.py
@@ -19,6 +19,14 @@ class DNSError(OSError):
 
 
 class AbstractBot:
+    """
+    This class is the core of SE Bot Checker. It handles the validation process. All
+    bot definitions should subclass this class.
+
+    A single bot class can be instantiated once and called many times. The allows
+    base settings to be configured and multiple IP and user agent pairs to be
+    validated simply.
+    """
     name = ''
     ips = []
     domains = []
@@ -31,10 +39,28 @@ class AbstractBot:
     request_user_agent = None
 
     def __init__(self, use_reverse_dns: bool = None):
+        """
+        The bot class constructor method.
+
+        :param use_reverse_dns: ``True`` if DNS requests should be made. This can slow
+            down the validation. However, for most crawlers this is the only way to
+            verify the validity of a crawler IP. You can turn this off if you want to
+            quickly match against a list of known valid IPs.
+        :type use_reverse_dns: bool
+        """
         if use_reverse_dns is not None:
             self.use_reverse_dns = use_reverse_dns
 
     def __call__(self, ip: str, user_agent: str) -> Tuple[bool, str]:
+        """
+        This method runs the validation.
+
+        :param ip: This is the IP of the crawler to validate.
+        :type ip: str
+        :param user_agent: This is the user agent string of the crawler.
+        :type user_agent: str
+        :return: Tuple[bool, str] --
+        """
         self.request_ip = ip
         self.request_user_agent = user_agent.lower()
         return self.run()
@@ -42,6 +68,7 @@ class AbstractBot:
     def run(self) -> Tuple[bool, str]:
         """
         Run the bot validation.
+
         :return: Tuple[bool, str]
         """
         # Test: 1 - User Agent Match
@@ -70,11 +97,26 @@ class AbstractBot:
         return True, self.name
 
     def valid_user_agent(self) -> bool:
+        """
+        Checks if the ``request_user_agent`` matches the bot ``user_agent`` signature.
+
+        If ``use_regex`` is ``True``, a RegEx search will be performed. Otherwise
+        simple substring matching will be used.
+
+        :return: bool -- True if ``request_user_agent`` matches the signature.
+        """
         if self.use_regex:
             return bool(re.search(self.user_agent, self.request_user_agent))
         return self.user_agent in self.request_user_agent
 
     def valid_domain(self, host: str) -> bool:
+        """
+        Checks if the ``host`` is matches a valid domain or super domain.
+
+        :param host: The host name form :func:`reverse_dns`.
+        :type host: str
+        :return:
+        """
         match = False
         for domain in self.domains:
             if host.endswith(domain):
@@ -82,14 +124,23 @@ class AbstractBot:
         return match
 
     def valid_ip(self) -> bool:
+        """
+        Checks if the ``request_ip`` is in the list of valid IPs, ``ips``.
+
+        :return: bool -- True if ``request_ip`` is in ``ips``
+        """
         if not self.ips:
             return False
         return self.request_ip in self.ips
 
     def reverse_dns(self) -> str:
         """
+        Performs a reverse DNS query based on the ``request_ip``
 
-        :return:
+        If there is a network error or the server IP is unreachable a :class:`DNSError`
+        error will be raised.
+
+        :return: str -- The host for the ``request_ip``
         :raises: DNSError
         """
         try:
@@ -100,8 +151,12 @@ class AbstractBot:
 
     def forward_dns(self, host) -> bool:
         """
+        Performs a forward DNS query based on the ``host``
 
-        :return:
+        If there is a network error or the server IP is unreachable a :class:`DNSError`
+        error will be raised.
+
+        :return: bool -- ``True`` if the forward DNS IP and ``request_ip`` match.
         :raises: DNSError
         """
         try:
@@ -112,8 +167,30 @@ class AbstractBot:
 
 
 class InstantBot(AbstractBot):
+    """
+    Use this class to quickly create a new bot without needing to subclass
+    ``AbstractBot``.
+    """
     def __init__(self, name: str, user_agent: str, domains: List[str] = [], use_regex: bool = False,
-                 use_reverse_dns: bool = None):
+                 use_reverse_dns: bool = True):
+        """
+        The new bot type constructor method.
+
+        :param name: The name of the new bot to return if valid.
+        :type name: str
+        :param user_agent: The user agent signature to match ``request_user_agent``
+            strings against.
+        :type user_agent: str
+        :param domains: A list of valid domains that the reverse DNS host can match.
+            Defaults to an empty list.
+        :type domains: List[str]
+        :param use_regex: ``True`` if you want to use RegEx to match the user agent.
+            Defaults to ``False``.
+        :type use_regex: bool
+        :param use_reverse_dns: ``True`` if you want SE Bot Checker to make DNS
+            requests to validate crawler IPs. Defaults to ``True``.
+        :type use_reverse_dns: bool
+        """
         super().__init__(use_reverse_dns=use_reverse_dns)
         self.name = name
         self.user_agent = user_agent
@@ -122,12 +199,18 @@ class InstantBot(AbstractBot):
 
 
 class GoogleBot(AbstractBot):
+    """
+    A prebuilt GoogleBot bot checker class.
+    """
     name = 'googlebot'
     domains = ['googlebot.com', 'google.com']
     user_agent = 'googlebot'
 
 
 class BingBot(AbstractBot):
+    """
+    A prebuilt BingBot bot checker class.
+    """
     name = 'bingbot'
     domains = ['search.msn.com']
     user_agent = r'bingbot|msnbot|bingpreview'
@@ -135,6 +218,9 @@ class BingBot(AbstractBot):
 
 
 class YandexBot(AbstractBot):
+    """
+    A prebuilt YandexBot bot checker class.
+    """
     name = 'yandexbot'
     domains = ['yandex.ru', 'yandex.net', 'yandex.com']
     user_agent = r'yandex|yadirectfetcher'
@@ -142,6 +228,9 @@ class YandexBot(AbstractBot):
 
 
 class DuckDuckBot(AbstractBot):
+    """
+    A prebuilt DuckDuckBot bot checker class.
+    """
     name = 'duckduckbot'
     ips = [
             '23.21.227.69',
